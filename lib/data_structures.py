b4435623f218e90a129f488dc70f21ffb4733931
nicholas.kiama <nicholas.kiama@student.moringaschool.com>
2025-08-22 09:51:39 +0300

    complete

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,52 +1,27 @@
-spicy_foods = [
-    {
-        "name": "Green Curry",
-        "cuisine": "Thai",
-        "heat_level": 9,
-    },
-    {
-        "name": "Buffalo Wings",
-        "cuisine": "American",
-        "heat_level": 3,
-    },
-    {
-        "name": "Mapo Tofu",
-        "cuisine": "Sichuan",
-        "heat_level": 6,
-    },
-]
-
 def get_names(spicy_foods):
     return [food["name"] for food in spicy_foods]
-
 
 def get_spiciest_foods(spicy_foods):
     return [food for food in spicy_foods if food["heat_level"] > 5]
 
-
-
 def print_spicy_foods(spicy_foods):
     for food in spicy_foods:
-        heat_emoji="🌶"* food ["heat_level"]
-        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_emoji}")
-
+        heat_emojis = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_emojis}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    return [food for food in spicy_foods if food["cuisine"] == cuisine][0]
-
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+    return None
 
 def print_spiciest_foods(spicy_foods):
-    for food in spicy_foods:
-        if food["heat_level"] > 5 :
-            heat_emoji ="🌶" * food["heat_level"]
-            print(f"{food["name"]} ({food["cuisine"]}) | Heat Level: {heat_emoji}")
+    spiciest = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(spiciest)
 
 def get_average_heat_level(spicy_foods):
-    for food in spicy_foods:
-       total=sum(food["heat_level"] for food in spicy_foods)
-       return total // len(spicy_foods)
-
+    total = sum(food["heat_level"] for food in spicy_foods)
+    return total // len(spicy_foods)
 
 def create_spicy_food(spicy_foods, spicy_food):
-    spicy_foods.append(spicy_food)
-    return spicy_foods
+    return spicy_foods + [spicy_food] 
